d59c19a77f90ca224c42e7c1fe331f4a228dc6a8
Miguel Ángel González Manchado <miguel-angel.gonzalez-manchado@capgemini.com>
2022-02-24 20:22:26 +0100

    feat: activate pins to go up/down or stop

--- VeluxControl/VeluxControl.ino
+++ VeluxControl/VeluxControl.ino
@@ -12,29 +12,31 @@ AsyncWebServer server(80);
 
 // -----------------------------------------------------------------------------
 
-#define FIRST_CONTROLLER_PIN_GREEN 15
-#define FIRST_CONTROLLER_PIN_RED 13
-#define FIRST_CONTROLLER_PIN_BLUE 12
+#define FIRST_CONTROLLER_PIN_UP 15
+#define FIRST_CONTROLLER_PIN_STOP 13
+#define FIRST_CONTROLLER_PIN_DOWN 12
 
-#define SECOND_CONTROLLER_PIN_GREEN 0
-#define SECOND_CONTROLLER_PIN_RED 4
-#define SECOND_CONTROLLER_PIN_BLUE 5
+#define SECOND_CONTROLLER_PIN_UP 0
+#define SECOND_CONTROLLER_PIN_STOP 4
+#define SECOND_CONTROLLER_PIN_DOWN 5
 
-#define SERIAL_BAUDRATE     115200
+#define SERIAL_BAUDRATE 115200
 
 IPAddress ip(192, 168, 1, 200);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
 
-const char* PARAM_MESSAGE = "window";
+const char *PARAM_MESSAGE = "window";
 
-int actionToExecute = 0;
+int window = 0; // FIRST == 1; SECOND == 2
+int action = 0; // UP == 1; DOWN == 2; STOP == 3
 
 // -----------------------------------------------------------------------------
 // Wifi
 // -----------------------------------------------------------------------------
 
-void wifiSetup() {
+void wifiSetup()
+{
 
   // Set WIFI module to STA mode
   WiFi.mode(WIFI_STA);
@@ -46,7 +48,8 @@ void wifiSetup() {
   WiFi.begin(WIFI_SSID, WIFI_PASS);
 
   // Wait
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print(".");
     delay(100);
   }
@@ -54,78 +57,114 @@ void wifiSetup() {
 
   // Connected!
   Serial.printf("[WIFI] STATION Mode, SSID: %s, IP address: %s\n", WiFi.SSID().c_str(), WiFi.localIP().toString().c_str());
-
 }
 
-void serverSetup() {
+void serverSetup()
+{
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(200, "text/plain", "Hello, world"); });
 
-  // Custom entry point (not required by the library, here just as an example)
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
-    request->send(200, "text/plain", "Hello, world");
-  });
+  server.on("/up", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+              String message;
+              if (request->hasParam(PARAM_MESSAGE)) {
+                message = request->getParam(PARAM_MESSAGE)->value();
+                if (message == "FIRST") {
+                  Serial.println("FIRST");
+                  window = 1;
+                } else if (message == "SECOND") {
+                  Serial.println("SECOND");
+                  window = 2;
+                } else {
+                  message = "Unknown device";
+                  window = 0;
+                }
 
-  server.on("/up", HTTP_GET, [](AsyncWebServerRequest * request) {
-    String message;
-    if (request->hasParam(PARAM_MESSAGE)) {
-      message = request->getParam(PARAM_MESSAGE)->value();
-      if (message == "FIRST") {
-        Serial.println("FIRST");
-        actionToExecute = 1;
-      } else if (message == "SECOND") {
-        Serial.println("SECOND");
-        actionToExecute = 2;
-      } else {
-        message = "Unknown device";
-      }
-    } else {
-      message = "No message sent";
-    }
-    request->send(200, "text/plain", "Hello, GET: " + message);
-  });
+                action = 1;
+              } else {
+                message = "No message sent";
+                window = 0;
+              }
+              request->send(200, "text/plain", "Hello, GET: " + message); });
+  
+  server.on("/down", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+              String message;
+              if (request->hasParam(PARAM_MESSAGE)) {
+                message = request->getParam(PARAM_MESSAGE)->value();
+                if (message == "FIRST") {
+                  Serial.println("FIRST");
+                  window = 1;
+                } else if (message == "SECOND") {
+                  Serial.println("SECOND");
+                  window = 2;
+                } else {
+                  message = "Unknown device";
+                  window = 0;
+                }
 
-  //    // These two callbacks are required for gen1 and gen3 compatibility
-  //    server.onRequestBody([](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
-  //        if (fauxmo.process(request->client(), request->method() == HTTP_GET, request->url(), String((char *)data))) return;
-  //        // Handle any other body request here...
-  //    });
-  //    server.onNotFound([](AsyncWebServerRequest *request) {
-  //        String body = (request->hasParam("body", true)) ? request->getParam("body", true)->value() : String();
-  //        if (fauxmo.process(request->client(), request->method() == HTTP_GET, request->url(), body)) return;
-  //        // Handle not found request here...
-  //    });
+                action = 2;
+              } else {
+                message = "No message sent";
+                window = 0;
+              }
+              request->send(200, "text/plain", "Hello, GET: " + message); });
+    server.on("/stop", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+              String message;
+              if (request->hasParam(PARAM_MESSAGE)) {
+                message = request->getParam(PARAM_MESSAGE)->value();
+                if (message == "FIRST") {
+                  Serial.println("FIRST");
+                  window = 1;
+                } else if (message == "SECOND") {
+                  Serial.println("SECOND");
+                  window = 2;
+                } else {
+                  message = "Unknown device";
+                  window = 0;
+                }
+
+                action = 3;
+              } else {
+                message = "No message sent";
+                window = 0;
+              }
+              request->send(200, "text/plain", "Hello, GET: " + message); });
 
   // Start the server
   server.begin();
-
 }
 
-void initVeluxPins() {
-  digitalWrite(FIRST_CONTROLLER_PIN_GREEN, LOW);
-  digitalWrite(FIRST_CONTROLLER_PIN_RED, LOW);
-  digitalWrite(FIRST_CONTROLLER_PIN_BLUE, LOW);
+void initVeluxPins()
+{
+  digitalWrite(FIRST_CONTROLLER_PIN_UP, LOW);
+  digitalWrite(FIRST_CONTROLLER_PIN_STOP, LOW);
+  digitalWrite(FIRST_CONTROLLER_PIN_DOWN, LOW);
 
-  digitalWrite(SECOND_CONTROLLER_PIN_GREEN, LOW);
-  digitalWrite(SECOND_CONTROLLER_PIN_RED, LOW);
-  digitalWrite(SECOND_CONTROLLER_PIN_BLUE, LOW);
+  digitalWrite(SECOND_CONTROLLER_PIN_UP, LOW);
+  digitalWrite(SECOND_CONTROLLER_PIN_STOP, LOW);
+  digitalWrite(SECOND_CONTROLLER_PIN_DOWN, LOW);
 
   // SETUP BUILT IN LED
   digitalWrite(LED_BUILTIN, HIGH);
 }
 
-void setup() {
+void setup()
+{
   // Init serial port and clean garbage
   Serial.begin(SERIAL_BAUDRATE);
   Serial.println();
   Serial.println();
 
   // SETUP CONTROLLER PINS
-  pinMode(FIRST_CONTROLLER_PIN_GREEN, OUTPUT);
-  pinMode(FIRST_CONTROLLER_PIN_RED, OUTPUT);
-  pinMode(FIRST_CONTROLLER_PIN_BLUE, OUTPUT);
+  pinMode(FIRST_CONTROLLER_PIN_UP, OUTPUT);
+  pinMode(FIRST_CONTROLLER_PIN_STOP, OUTPUT);
+  pinMode(FIRST_CONTROLLER_PIN_DOWN, OUTPUT);
 
-  pinMode(SECOND_CONTROLLER_PIN_GREEN, OUTPUT);
-  pinMode(SECOND_CONTROLLER_PIN_RED, OUTPUT);
-  pinMode(SECOND_CONTROLLER_PIN_BLUE, OUTPUT);
+  pinMode(SECOND_CONTROLLER_PIN_UP, OUTPUT);
+  pinMode(SECOND_CONTROLLER_PIN_STOP, OUTPUT);
+  pinMode(SECOND_CONTROLLER_PIN_DOWN, OUTPUT);
 
   pinMode(LED_BUILTIN, OUTPUT);
 
@@ -137,15 +176,65 @@ void setup() {
 
   // Web server
   serverSetup();
-
 }
 
-void loop() {
-  // This is a sample code to output free heap every 5 seconds
-  // This is a cheap way to detect memory leaks
-  static unsigned long last = millis();
-  if (millis() - last > 5000) {
-    last = millis();
-    Serial.printf("[MAIN] Free heap: %d bytes\n", ESP.getFreeHeap());
+void loop()
+{
+  if (window == 0)
+  {
+    Serial.println("INIT");
+    window = -1;
+    action = -1;
+    initVeluxPins();
+  }
+
+  if (window == 1)
+  {
+    Serial.print("FIRST --> ");
+    Serial.println(action);
+    window = 0;
+    
+    switch (action)
+    {
+      case 1:
+        digitalWrite(FIRST_CONTROLLER_PIN_UP, HIGH);
+        break;
+      
+      case 2:
+        digitalWrite(FIRST_CONTROLLER_PIN_DOWN, HIGH);
+        break;
+      
+      default:
+        digitalWrite(FIRST_CONTROLLER_PIN_STOP, HIGH);
+        break;
+    }
+
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
+  }
+
+  if (window == 2)
+  {
+    Serial.print("SECOND --> ");
+    Serial.println(action);
+    window = 0;
+
+    switch (action)
+    {
+      case 1:
+        digitalWrite(SECOND_CONTROLLER_PIN_UP, HIGH);
+        break;
+      
+      case 2:
+        digitalWrite(SECOND_CONTROLLER_PIN_DOWN, HIGH);
+        break;
+      
+      default:
+        digitalWrite(SECOND_CONTROLLER_PIN_STOP, HIGH);
+        break;
+    }
+
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
   }
 }
